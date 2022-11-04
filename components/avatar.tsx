b6a1db825171801147c9433dbb432fb47f2c3a89
Jake Funke <jifunks@gmail.com>
2022-11-03 17:13:41 -0700

    Looking pretty good

---
+++ components/avatar.tsx
@@ -0,0 +1,4 @@
+import styles from './avatar.module.css'
+export default function Avatar() {
+  return (<div className={styles.photo}></div>)
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Ubuntu } from "@next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const ubuntu = Ubuntu({ weight: "400", subsets: ['latin'] });
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${ubuntu.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
