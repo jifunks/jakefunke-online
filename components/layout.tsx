1a3a3baaa478ab40bead2615cce93ebdc9cd0b31
Jake Funke <jifunks@gmail.com>
2022-11-03 21:37:34 -0700

    layout better

---
+++ components/layout.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/home.module.css";
+import Divider from "./divider";
+
+const name = "Jake Funke";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({
+  children,
+  home,
+  title,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+  title?: string;
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{home ? "Jake Funke" : `Jake Funke -  ${title}`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>{children}</main>
+
+      {!home && (
+        <>
+          <Divider />
+          <div className={styles.backToHome}>
+            <Link href="/">← Back to home</Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
+import Divider from "../components/divider";
+import Layout from "../components/layout";
+import styles from "../styles/home.module.css";
 
-export default function AboutMe({
-  postData,
-}: {
-  postData: {
-    title: string;
-    date: string;
-    contentHtml: string;
-  };
-}) {
+export default function AboutMe({}) {
   return (
-    <>
+    <Layout title={"About Me"}>
+      <h1 className={styles.title}>About Me</h1>
+      <Divider></Divider>
       dangerosly cheesy<br></br>
-      <Link href="/">← Back to home</Link>
-    </>
+    </Layout>
   );
 }
