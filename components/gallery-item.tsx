380314e09c4e3272c3f2969177f90aa2c3e04756
Jake Funke <jifunks@gmail.com>
2022-11-07 14:08:31 -0800

    update components on click

--- components/gallery-item.tsx
+++ components/gallery-item.tsx
@@ -1,15 +1,22 @@
 import style from "./Gallery.module.css";
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 
-export default function GalleryItem({ imageUrl }: { imageUrl?: string }) {
-  function onClick() {
-    console.log("hot dog time");
-  }
+export default function GalleryItem({
+  onClick,
+  imageUrl,
+}: {
+  onClick: MouseEventHandler<HTMLDivElement>;
+  imageUrl?: string;
+}) {
   return (
-    <div onClick={onClick} className={style["gallery-item"]}>
+    <div className={style["gallery-item"]} onClick={onClick}>
       <Image
+        // src={
+        //   imageUrl ??
+        //   "https://images.unsplash.com/photo-1619538189873-3f511db5fec6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+        // }
         src={
-          imageUrl ??
           "https://images.unsplash.com/photo-1619538189873-3f511db5fec6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
         }
         alt={""}
--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,17 +1,45 @@
+import React from "react";
 import GalleryItem from "./gallery-item";
 import style from "./Gallery.module.css";
+import Image from "next/image";
 
 export default function Gallery({}) {
+  const [openImage, setOpenImage] = React.useState(null);
+
+  const galleryItems = ["1", "2", "3", "4", "5", "6"];
+
+  function setImage(image: any) {
+    console.log(`clicked image ${image}`);
+    setOpenImage(image);
+    return;
+  }
+
   return (
-    <div className={style.gallery}>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-      <GalleryItem></GalleryItem>
-    </div>
+    <>
+      <div className={style.gallery}>
+        {galleryItems.map((imagePath, i) => (
+          <GalleryItem
+            imageUrl={imagePath}
+            key={i}
+            onClick={() => setImage(i)}
+          />
+        ))}
+      </div>
+      {openImage !== null && (
+        <div className={style["hero-image"]}>
+          <Image
+            src={
+              "https://images.unsplash.com/photo-1619538189873-3f511db5fec6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+            }
+            alt={""}
+            width={500}
+            height={500}
+            onClick={() => {
+              setOpenImage(null);
+            }}
+          ></Image>
+        </div>
+      )}
+    </>
   );
 }
