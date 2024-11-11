eb7bdce89e049a202c81420bc685a9d276212d6e
Jake Funke <jifunks@gmail.com>
2024-11-10 22:45:12 -0800

    new job

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function About({}) {
   return (
     <Layout title={"About"}>
-      This site was built with Next.js, all by hand. Peep the source {` `}
+      This site was built with Next.js. Was it overkill? Absolutely. I just wanted to try it out. You can find the source code for this site{" "}
       <a href="https://github.com/jifunks/jakefunke-online">here</a>.
     </Layout>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <div className={styles.content}>
         <p>
           Hello! I'm Jake, and I love building web applications. I'm currently a
-          Senior Software Engineer at <a href="https://artera.io">Artera</a>.
+          Senior Software Engineer at <a href="https://www.wellthapp.com">Wellth</a>.
         </p>
         <p>
           I also made <a href="https://github.com/jifunks/botany">botany</a> - a
